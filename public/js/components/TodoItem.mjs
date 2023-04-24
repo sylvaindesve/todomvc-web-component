dfa7815b0e3a7f6903aef57b4512c66c3d80e08c
Sylvain Desvé <sylvain.desve@gmail.com>
2023-04-24 21:50:54 +0200

    Refactoring TodoItem

--- public/js/components/TodoItem.mjs
+++ public/js/components/TodoItem.mjs
@@ -1,197 +1,206 @@
+const style = `
+:host {
+  display: block;
+}
+.view {
+  display: flex;
+  padding: 1rem;
+}
+.completed {
+  margin: auto 0.2rem;
+  height: 100%;
+}
+.completed:checked + .description {
+  text-decoration: line-through;
+}
+.description {
+  margin: auto 0.2rem;
+}
+:hover .delete {
+  visibility: visible;
+}
+.delete {
+  appearance: none;
+  transition: color 0.2s ease-out;
+  visibility: hidden;
+  margin: auto 0.2rem;
+  margin-left: auto;
+  border: 0;
+  background: none;
+  padding: 0;
+  color: #cc9a9a;
+  font-family: inherit;
+  font-size: inherit;
+  font-weight: inherit;
+}
+.delete:hover {
+  color: #af5b5e;
+}
+.edit {
+  color: inherit;
+  font-family: inherit;
+  font-size: inherit;
+  font-weight: inherit;
+  box-sizing: border-box;
+  width: 100%;
+  padding: 1rem;
+}
+.edit:focus {
+  outline: 0;
+}
+.hide {
+  display: none;
+}
+`;
+
+const template = document.createElement("template");
+template.innerHTML = `
+<style>${style}</style>
+<div class="view">
+  <input class="completed" type="checkbox" />
+  <label class="description"></label>
+  <button class="delete">X</button>
+</div>
+<input class="edit hide" />
+`;
+
 export class TodoItem extends HTMLElement {
-  static get style() {
-    return `
-    .view {
-      display: flex;
-      padding: 1rem;
-    }
-    .completed {
-      margin: auto 0.2rem;
-      height: 100%;
-    }
-    .completed:checked + .description {
-      text-decoration: line-through;
-    }
-    .description {
-      margin: auto 0.2rem;
-    }
-    :hover .delete {
-      visibility: visible;
-    }
-    .delete {
-      appearance: none;
-      transition: color 0.2s ease-out;
-      visibility: hidden;
-      margin: auto 0.2rem;
-      margin-left: auto;
-      border: 0;
-      background: none;
-      padding: 0;
-      color: #cc9a9a;
-      font-family: inherit;
-      font-size: inherit;
-      font-weight: inherit;
-    }
-    .delete:hover {
-      color: #af5b5e;
-    }
-    .edit {
-      color: inherit;
-      font-family: inherit;
-      font-size: inherit;
-      font-weight: inherit;
-      box-sizing: border-box;
-      width: 100%;
-      padding: 1rem;
-    }
-    .edit:focus {
-      outline: 0;
-    }
-    .hide {
-      display: none;
-    }
-    `;
-  }
-
-  static get template() {
-    const template = document.createElement("template");
-    template.innerHTML = `
-    <style>${this.style}</style>
-    <div class="view">
-      <input class="completed" type="checkbox" />
-      <label class="description"></label>
-      <button class="delete">X</button>
-    </div>
-    <input class="edit hide" />
-    `;
-    return template;
-  }
-
   static get observedAttributes() {
     return ["description", "completed"];
   }
 
-  #state = {
-    editing: false,
-    newDescription: "",
-  };
+  // Nodes
+  #viewSection;
+  #descriptionLabel;
+  #completedCheckbox;
+  #deleteButton;
+  #editDescriptionInput;
+
+  // Properties
+  #editing = false;
+  #editDescription = "";
+
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+    this.#descriptionLabel = this.shadowRoot.querySelector(".description");
+    this.#completedCheckbox = this.shadowRoot.querySelector(".completed");
+    this.#deleteButton = this.shadowRoot.querySelector(".delete");
+    this.#viewSection = this.shadowRoot.querySelector(".view");
+    this.#editDescriptionInput = this.shadowRoot.querySelector(".edit");
+  }
 
   connectedCallback() {
-    this.attachShadow({ mode: "open" });
-    this.shadowRoot.appendChild(
-      this.constructor.template.content.cloneNode(true)
-    );
-
-    // Handlers
-
-    this.completedCheckbox.addEventListener("click", () => {
-      if (this.completed) {
-        this.dispatchEvent(
-          new CustomEvent("todo-item-to-do", { bubbles: true })
-        );
-      } else {
-        this.dispatchEvent(
-          new CustomEvent("todo-item-completed", { bubbles: true })
-        );
-      }
+    this.#completedCheckbox.addEventListener("change", (event) => {
+      const success = this.#dispatchCompletionOrUncompletion();
+      if (!success) event.preventDefault();
     });
-
-    this.descriptionLabel.addEventListener("dblclick", () => {
-      this.#state.editing = true;
-      this.#state.newDescription = this.description;
-      this.update("state.editing", "state.newDescription");
+    this.#descriptionLabel.addEventListener("dblclick", () => {
+      this.editDescription = this.description;
+      this.editing = true;
     });
-
-    this.deleteButton.addEventListener("click", (event) => {
+    this.#deleteButton.addEventListener("click", (event) => {
       event.preventDefault();
-      this.dispatchEvent(
-        new CustomEvent("todo-item-deleted", { bubbles: true })
-      );
+      this.#dispatchDeletion();
     });
-
-    this.editDescriptionInput.addEventListener("keydown", (event) => {
+    this.#editDescriptionInput.addEventListener("keydown", (event) => {
       if (event.key === "Escape") {
-        this.#state.editing = false;
-        this.update("state.editing");
+        this.editing = false;
       }
-      this.#state.newDescription = this.editDescriptionInput.value;
+      this.editDescription = this.#editDescriptionInput.value;
     });
-
-    this.editDescriptionInput.addEventListener("change", () => {
-      this.dispatchEvent(
-        new CustomEvent("todo-item-description-updated", {
-          bubbles: true,
-          detail: this.editDescriptionInput.value,
-        })
-      );
-      this.#state.editing = false;
-      this.update("state.editing");
+    this.#editDescriptionInput.addEventListener("change", () => {
+      this.editDescription = this.#editDescriptionInput.value;
+      if (this.#dispatchDescriptionUpdate()) {
+        this.editing = false;
+      }
     });
-
-    this.update("completed", "description", "state.editing");
   }
 
   attributeChangedCallback(name) {
-    this.update(name);
+    if (name === "description") {
+      this.#descriptionLabel.innerHTML = this.description;
+    }
+
+    if (name === "completed") {
+      if (this.completed) {
+        this.#completedCheckbox.checked = true;
+      } else {
+        this.#completedCheckbox.checked = false;
+      }
+    }
+  }
+
+  set description(value) {
+    this.setAttribute("description", value);
   }
 
   get description() {
     return this.getAttribute("description");
   }
 
+  set completed(value) {
+    const isCompleted = Boolean(value);
+    if (isCompleted) this.setAttribute("completed", "");
+    else this.removeAttribute("completed");
+  }
+
   get completed() {
     return this.hasAttribute("completed");
   }
 
-  get descriptionLabel() {
-    return this.shadowRoot.querySelector(".description");
-  }
-
-  get completedCheckbox() {
-    return this.shadowRoot.querySelector(".completed");
-  }
-
-  get deleteButton() {
-    return this.shadowRoot.querySelector(".delete");
-  }
-
-  get viewSection() {
-    return this.shadowRoot.querySelector(".view");
-  }
-
-  get editDescriptionInput() {
-    return this.shadowRoot.querySelector(".edit");
-  }
-
-  update(...attrs) {
-    if (!this.shadowRoot) return;
-
-    if (attrs.includes("description")) {
-      this.descriptionLabel.innerHTML = this.description;
+  set editing(isEditing) {
+    this.#editing = isEditing;
+    if (isEditing) {
+      this.#viewSection.classList.add("hide");
+      this.#editDescriptionInput.classList.remove("hide");
+      this.#editDescriptionInput.focus();
+    } else {
+      this.#viewSection.classList.remove("hide");
+      this.#editDescriptionInput.classList.add("hide");
     }
+  }
 
-    if (attrs.includes("completed")) {
-      if (this.completed) {
-        this.completedCheckbox.setAttribute("checked", "checked");
-      } else {
-        this.completedCheckbox.removeAttribute("checked");
-      }
-    }
+  get editing() {
+    return this.#editing;
+  }
 
-    if (attrs.includes("state.editing")) {
-      if (this.#state.editing) {
-        this.viewSection.classList.add("hide");
-        this.editDescriptionInput.classList.remove("hide");
-        this.editDescriptionInput.focus();
-      } else {
-        this.viewSection.classList.remove("hide");
-        this.editDescriptionInput.classList.add("hide");
-      }
-    }
+  set editDescription(value) {
+    this.#editDescription = value;
+    this.#editDescriptionInput.value = value;
+  }
 
-    if (attrs.includes("state.newDescription")) {
-      this.editDescriptionInput.value = this.#state.newDescription;
+  get editDescription() {
+    return this.#editDescription;
+  }
+
+  #dispatchCompletionOrUncompletion() {
+    if (this.completed) {
+      return this.dispatchEvent(
+        new CustomEvent("todo-item-to-do", { bubbles: true })
+      );
+    } else {
+      return this.dispatchEvent(
+        new CustomEvent("todo-item-completed", { bubbles: true })
+      );
     }
+  }
+
+  #dispatchDeletion() {
+    return this.dispatchEvent(
+      new CustomEvent("todo-item-deleted", { bubbles: true })
+    );
+  }
+
+  #dispatchDescriptionUpdate() {
+    return this.dispatchEvent(
+      new CustomEvent("todo-item-description-updated", {
+        bubbles: true,
+        detail: this.editDescription,
+      })
+    );
   }
 }
 
--- test/components/TodoItem.test.mjs
+++ test/components/TodoItem.test.mjs
@@ -10,120 +10,137 @@ chai.use(sinonChai);
 describe("TodoItem", function () {
   beforeEach(function () {
     document.body.innerHTML = `<todo-item description="Quelque chose"></todo-item>`;
-    this.currentTest.todoItem = document.querySelector("todo-item");
-    this.currentTest.eventListenerCallback = fake();
+    this.todoItem = document.querySelector("todo-item");
+    this.eventListenerCallback = fake();
   });
   it("should be in view mode", function () {
-    expect(this.test.todoItem.viewSection.classList.contains("hide")).to.be
+    expect(this.todoItem.editing).to.be.false;
+    expect(getViewSection(this.todoItem).classList.contains("hide")).to.be
       .false;
-    expect(this.test.todoItem.editDescriptionInput.classList.contains("hide"))
-      .to.be.true;
+    expect(getEditDescriptionInput(this.todoItem).classList.contains("hide")).to
+      .be.true;
   });
 
   it("should render the description", function () {
-    expect(this.test.todoItem.descriptionLabel.innerHTML).to.equal(
+    expect(getDescriptionLabel(this.todoItem).innerHTML).to.equal(
       "Quelque chose"
     );
   });
 
   it("should update when description is changed", function () {
-    this.test.todoItem.setAttribute("description", "Autre chose");
-    expect(this.test.todoItem.descriptionLabel.innerHTML).to.equal(
+    this.todoItem.description = "Autre chose";
+    expect(getDescriptionLabel(this.todoItem).innerHTML).to.equal(
       "Autre chose"
     );
   });
 
   it("should not be checked if not completed", function () {
-    expect(this.test.todoItem.completedCheckbox.checked).to.be.false;
-    expect(this.test.todoItem.completedCheckbox.hasAttribute("checked")).to.be
-      .false;
+    expect(getCompletedCheckbox(this.todoItem).checked).to.be.false;
   });
 
   it("should be checked if completed", function () {
     document.body.innerHTML = `<todo-item description="Quelque chose" completed></todo-item>`;
     const todoItem = document.querySelector("todo-item");
-    expect(todoItem.completedCheckbox.checked).to.be.true;
-    expect(todoItem.completedCheckbox.hasAttribute("checked")).to.be.true;
+    expect(getCompletedCheckbox(todoItem).checked).to.be.true;
   });
 
   it("should update when completed attribute is changed", function () {
-    this.test.todoItem.setAttribute("completed", "completed");
-    expect(this.test.todoItem.completedCheckbox.checked).to.be.true;
-    this.test.todoItem.removeAttribute("completed");
-    expect(this.test.todoItem.completedCheckbox.checked).to.be.false;
+    this.todoItem.completed = true;
+    expect(getCompletedCheckbox(this.todoItem).checked).to.be.true;
+    this.todoItem.completed = false;
+    expect(getCompletedCheckbox(this.todoItem).checked).to.be.false;
   });
 
   it("should dispatch a 'todo-item-completed' event when the checkbox is checked", function () {
-    this.test.todoItem.addEventListener(
+    this.todoItem.addEventListener(
       "todo-item-completed",
-      this.test.eventListenerCallback
+      this.eventListenerCallback
     );
-    this.test.todoItem.completedCheckbox.click();
-    expect(this.test.eventListenerCallback).to.have.been.called;
+    getCompletedCheckbox(this.todoItem).click();
+    expect(this.eventListenerCallback).to.have.been.called;
   });
 
   it("should dispatch a 'todo-item-to-do' event when the checkbox is unchecked", function () {
     document.body.innerHTML = `<todo-item description="Quelque chose" completed></todo-item>`;
     const todoItem = document.querySelector("todo-item");
-    todoItem.addEventListener(
-      "todo-item-to-do",
-      this.test.eventListenerCallback
-    );
-    todoItem.completedCheckbox.click();
-    expect(this.test.eventListenerCallback).to.have.been.called;
+    todoItem.addEventListener("todo-item-to-do", this.eventListenerCallback);
+    getCompletedCheckbox(todoItem).click();
+    expect(this.eventListenerCallback).to.have.been.called;
   });
 
   it("should dispatch a 'todo-item-deleted' event when the delete button is clicked", function () {
-    this.test.todoItem.addEventListener(
+    this.todoItem.addEventListener(
       "todo-item-deleted",
-      this.test.eventListenerCallback
+      this.eventListenerCallback
     );
-    this.test.todoItem.deleteButton.click();
-    expect(this.test.eventListenerCallback).to.have.been.called;
+    getDeleteButton(this.todoItem).click();
+    expect(this.eventListenerCallback).to.have.been.called;
   });
 
   it("should switch to edit mode when the description is double-clicked", function () {
-    dblClick(this.test.todoItem.descriptionLabel);
-    expect(this.test.todoItem.viewSection.classList.contains("hide")).to.be
-      .true;
-    expect(this.test.todoItem.editDescriptionInput.classList.contains("hide"))
-      .to.be.false;
+    dblClick(getDescriptionLabel(this.todoItem));
+    expect(this.todoItem.editing).to.be.true;
+    expect(getViewSection(this.todoItem).classList.contains("hide")).to.be.true;
+    expect(getEditDescriptionInput(this.todoItem).classList.contains("hide")).to
+      .be.false;
   });
 
   it("should have the edit input initialized with current description", function () {
-    dblClick(this.test.todoItem.descriptionLabel);
-    expect(this.test.todoItem.editDescriptionInput.value).to.equal(
+    dblClick(getDescriptionLabel(this.todoItem));
+    expect(getEditDescriptionInput(this.todoItem).value).to.equal(
       "Quelque chose"
     );
   });
 
   it("should switch back to view mode when user enters Escape in the input", function () {
-    dblClick(this.test.todoItem.descriptionLabel);
-    enterKey(this.test.todoItem.editDescriptionInput, "Escape");
-    expect(this.test.todoItem.viewSection.classList.contains("hide")).to.be
+    dblClick(getDescriptionLabel(this.todoItem));
+    enterKey(getEditDescriptionInput(this.todoItem), "Escape");
+    expect(this.todoItem.editing).to.be.false;
+    expect(getViewSection(this.todoItem).classList.contains("hide")).to.be
       .false;
-    expect(this.test.todoItem.editDescriptionInput.classList.contains("hide"))
-      .to.be.true;
+    expect(getEditDescriptionInput(this.todoItem).classList.contains("hide")).to
+      .be.true;
   });
 
   it("should dispatch a 'todo-item-description-updated' event with new description and switch back to view mode", function () {
-    this.test.todoItem.addEventListener(
+    this.todoItem.addEventListener(
       "todo-item-description-updated",
-      this.test.eventListenerCallback
+      this.eventListenerCallback
     );
-    dblClick(this.test.todoItem.descriptionLabel);
-    this.test.todoItem.editDescriptionInput.value = "Nouvelle description";
-    this.test.todoItem.editDescriptionInput.dispatchEvent(new Event("change"));
-    expect(this.test.eventListenerCallback).to.have.been.called;
-    expect(this.test.eventListenerCallback.firstCall.firstArg.detail).to.equal(
+    dblClick(getDescriptionLabel(this.todoItem));
+    getEditDescriptionInput(this.todoItem).value = "Nouvelle description";
+    getEditDescriptionInput(this.todoItem).dispatchEvent(new Event("change"));
+    expect(this.eventListenerCallback).to.have.been.called;
+    expect(this.eventListenerCallback.firstCall.firstArg.detail).to.equal(
       "Nouvelle description"
     );
-    expect(this.test.todoItem.viewSection.classList.contains("hide")).to.be
+    expect(this.todoItem.editing).to.be.false;
+    expect(getViewSection(this.todoItem).classList.contains("hide")).to.be
       .false;
-    expect(this.test.todoItem.editDescriptionInput.classList.contains("hide"))
-      .to.be.true;
+    expect(getEditDescriptionInput(this.todoItem).classList.contains("hide")).to
+      .be.true;
   });
 });
+
+function getViewSection(todoItem) {
+  return todoItem.shadowRoot.querySelector(".view");
+}
+
+function getEditDescriptionInput(todoItem) {
+  return todoItem.shadowRoot.querySelector(".edit");
+}
+
+function getDescriptionLabel(todoItem) {
+  return todoItem.shadowRoot.querySelector(".description");
+}
+
+function getCompletedCheckbox(todoItem) {
+  return todoItem.shadowRoot.querySelector(".completed");
+}
+
+function getDeleteButton(todoItem) {
+  return todoItem.shadowRoot.querySelector(".delete");
+}
 
 function dblClick(node) {
   node.dispatchEvent(
